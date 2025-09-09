1b33aeccc5eddc800f7204939f4a4ac8587c551f
qescobar <quentin.escobar@etu.univ-cotedazur.fr>
2025-09-09 13:51:54 +0200

    :sparkles: pool de worker fonctionnelle

--- internal/worker/grpc_server.go
+++ internal/worker/grpc_server.go
@@ -3,7 +3,11 @@ package worker
 import (
 	"context"
 	pb "github.com/moustik06/healthchecker/gen/go/health"
+	"github.com/moustik06/healthchecker/internal/worker/pool"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
+	"time"
 )
 
 type Server struct {
@@ -12,7 +16,32 @@ type Server struct {
 
 func (s *Server) Check(ctx context.Context, req *pb.HealthCheckRequest) (*pb.HealthCheckResponse, error) {
 	log.Printf("Appel gRPC reçu pour Check urls : %d\n", len(req.Urls))
-	return &pb.HealthCheckResponse{
+
+	workerPool, err := pool.New(5, time.Second*10)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "échec de la création du pool: %v", err)
+	}
+
+	workerPool.Run(ctx)
+	go workerPool.GenerateJobs(req.Urls)
+	go workerPool.Wait()
+	response := &pb.HealthCheckResponse{
 		Results: make(map[string]*pb.HealthCheckResult),
-	}, nil
+	}
+
+	for result := range workerPool.Results() {
+		log.Printf("Résultat pour %s : %s \n", result.URL, result.Status)
+
+		checkResult := &pb.HealthCheckResult{}
+		if result.Status == "success" {
+			checkResult.Status = pb.CheckStatus_CHECK_STATUS_OK
+		} else {
+			checkResult.Status = pb.CheckStatus_CHECK_STATUS_ERROR
+			checkResult.ErrorMessage = result.ErrorMsg
+		}
+		response.Results[result.URL] = checkResult
+	}
+
+	log.Println("Toutes les vérifications sont terminées.")
+	return response, nil
 }
---
+++ internal/worker/pool/pool.go
@@ -0,0 +1,110 @@
+package pool
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+/**
+ * A quoi va servir plusieurs workers ?
+ * - Gérer plusieurs requêtes HTTP en parallèle
+ * - Améliorer la latence globale
+ * - Augmenter le débit (nombre de requêtes traitées par seconde)
+ * - Répartir la charge sur le serveur cible
+ * - Gérer les pics de trafic
+
+ * De quoi a besoin un worker ?
+ * - D'une URL à vérifier -> un job
+ * - Pouvoir fournir un résultat -> Meme Result que protobuf ?
+ * - D'un client HTTP pour faire la requête
+ */
+
+type Job struct {
+	URL string
+}
+
+type Result struct {
+	URL      string
+	Status   string // "success" ou "failure"
+	ErrorMsg string // Message d'erreur si failure
+}
+
+type WorkerPool struct {
+	numWorkers int
+	jobs       chan Job
+	results    chan Result
+	wg         sync.WaitGroup
+	httpClient *http.Client
+}
+
+func New(numWorkers int, clientTimeout time.Duration) (*WorkerPool, error) {
+	pool := &WorkerPool{
+		numWorkers: numWorkers,
+		jobs:       make(chan Job, numWorkers*5), // x5 pour avoir assez de buffer si on envoie beaucoup de jobs d'un coup
+		results:    make(chan Result, numWorkers*5),
+		httpClient: &http.Client{
+			Timeout: clientTimeout,
+		},
+	}
+
+	return pool, nil
+}
+
+func (wp *WorkerPool) worker(ctx context.Context, id int) {
+	defer wp.wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Worker s'arrête car le contexte a été annulé. ( timeout ou annulation )")
+			return
+		case job, ok := <-wp.jobs:
+			if !ok {
+				return
+			}
+			start := time.Now() // Pour mesurer la durée de la requête
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, job.URL, nil)
+
+			if err != nil {
+				wp.results <- Result{URL: job.URL, Status: "failure", ErrorMsg: err.Error()}
+				continue // On passe au job suivant
+			}
+			resp, err := wp.httpClient.Do(req)
+			duration := time.Since(start) // Durée de la requête
+
+			if err != nil {
+				log.Printf("ERREUR de vérification pour %s: %v", job.URL, err)
+				wp.results <- Result{URL: job.URL, Status: "failure", ErrorMsg: err.Error()}
+				continue // On passe au job suivant
+			}
+			resp.Body.Close()
+			log.Printf("Vérification réussie pour %s en %v", job.URL, duration)
+			wp.results <- Result{URL: job.URL, Status: "success"}
+
+		}
+	}
+}
+
+// Run /** démarre le pool de workers en lançant les goroutines. */
+func (wp *WorkerPool) Run(ctx context.Context) {
+	for i := 0; i < wp.numWorkers; i++ {
+		wp.wg.Add(1)
+		go wp.worker(ctx, i)
+	}
+}
+func (wp *WorkerPool) GenerateJobs(urls []string) {
+	for _, url := range urls {
+		wp.jobs <- Job{URL: url}
+	}
+	close(wp.jobs) // On ferme le canal des jobs une fois tous les jobs envoyés
+}
+
+func (wp *WorkerPool) Wait() {
+	wp.wg.Wait()      // On attend que tous les workers aient terminé
+	close(wp.results) // On ferme le canal des résultats
+}
+func (wp *WorkerPool) Results() <-chan Result {
+	return wp.results
+}
